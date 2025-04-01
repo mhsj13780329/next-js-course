b2bd3c427efc94e44b2a92b31f7e5de280e5a9ec
Hanif <mhanifsaadaeij@gmail.com>
2025-04-01 23:27:52 +0330

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { inter } from "./ui/fonts";
+import "@/app/ui/global.css";
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
